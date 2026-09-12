f133598925b92af1def6e1eb7971b8d3040aaf38
xdd2024030565 <xdd2024030565@gmail.com>
2026-09-12 23:33:26 +0800

    Fix fabric-loom version from 1.5-SNAPSHOT to 1.17 (latest stable version)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     // Fabric Loom (Mod 编译)
-    id("fabric-loom") version "1.5-SNAPSHOT" apply false
+    id("fabric-loom") version "1.17" apply false  // 修复：使用稳定版本
 }
